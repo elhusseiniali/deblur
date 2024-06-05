2944c60444222fba5224c3a9255ded06a64ed9cd
Ali El Husseini <elhusseiniali@icloud.com>
2024-06-05 16:10:34 +0800

    Add support for 3 images in plot_sample() by checking if a third image is inside the passed tuple.

--- utils.py
+++ utils.py
@@ -1,17 +1,38 @@
 import matplotlib.pyplot as plt
 import torch
 
+import numpy as np
 
-def plot_sample(sample, figsize=(10, 5)):
+
+mean = np.array([0.485, 0.456, 0.406])
+std = np.array([0.229, 0.224, 0.225])
+
+
+def plot_sample(sample, figsize=(10, 5), normalized=False):
     plt.figure(figsize=figsize)
 
-    plt.subplot(1, 2, 1)
-    plt.imshow(sample[0].permute(*torch.arange(sample[0].ndim - 1, -1, -1)))
-    plt.title('blur')
+    num_cols = len(sample)
+    plt.subplot(1, num_cols, 1)
+    blurry_image = sample[0].permute(*torch.arange(sample[0].ndim - 1, -1, -1))
+    if normalized:
+        blurry_image = (blurry_image * std) + mean
+    plt.imshow(blurry_image)
+    plt.title('blurry image')
 
-    plt.subplot(1, 2, 2)
-    plt.imshow(sample[1].permute(*torch.arange(sample[0].ndim - 1, -1, -1)))
-    plt.title('sharp')
+    plt.subplot(1, num_cols, 2)
+    sharp_image = sample[1].permute(*torch.arange(sample[1].ndim - 1, -1, -1))
+    if normalized:
+        sharp_image = (sharp_image * std) + mean
+    plt.imshow(sharp_image)
+    plt.title('sharp image')
+
+    if num_cols == 3:
+        plt.subplot(1, num_cols, 3)
+        prediction = sample[2].permute(*torch.arange(sample[2].ndim - 1, -1, -1))
+        if normalized:
+            prediction = (prediction * std) + mean
+        plt.imshow(prediction)
+        plt.title('model prediction')
 
     plt.show()
 
