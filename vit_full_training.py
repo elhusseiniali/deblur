0f3a3f93dd509c2a6eb9036576041b60090e6caf
Ali El Husseini <elhusseiniali@icloud.com>
2024-07-08 14:18:14 +0800

    Update file to train vit_b32 on best evaluation params.

--- vit_full_training.py
+++ vit_full_training.py
@@ -7,45 +7,40 @@ from vit import get_vit, SUPPORTED_VIT
 
 from trainer import Trainer
 
-from loss import get_loss, SUPPORTED_LOSS
-from optimizer import get_optimizer, SUPPORTED_OPTIMIZERS
+from loss import get_loss
+from optimizer import get_optimizer
 
 
-celeb_path = Path('./data/CelebA')
-train_path = celeb_path / 'train'
-test_path = celeb_path / 'test'
-val_path = celeb_path / 'validation'
+if __name__ == '__main__':
+    celeb_path = Path('./data/CelebA')
+    train_path = celeb_path / 'train'
+    test_path = celeb_path / 'test'
+    val_path = celeb_path / 'validation'
 
-batch_size = 256
-image_limit = None
+    batch_size = 256
+    image_limit = None
 
-learning_rates = [
-    1e-6,
-    #    1e-4,
-    #    1e-2
-]
-num_epochs = 50
+    learning_rate = 1e-4
+    num_epochs = 60
 
-debug = False
-debug_step = 15
-debug_image_count = 2
+    debug = False
+    debug_step = 10
+    debug_image_count = 2
 
-workers = 48
+    workers = 48
+    AUGMENT_DATA = True
 
-AUGMENT_DATA = True
-
-print(f'Supported vision transformers: {list(SUPPORTED_VIT.keys())}')
-print(f'Supported losses: {list(SUPPORTED_LOSS.keys())}')
-print(f'Supported optimizers: {list(SUPPORTED_OPTIMIZERS.keys())}')
-
-models = [
-    'vit_l32'
-]
-for model_id in models:
+    model_id = 'vit_b32'
     assert model_id in SUPPORTED_VIT.keys()
+    criterion_id = 'L1'
+    optimizer_id = 'adam'
 
-for model_id in models:
-    print(f'Using model: {model_id}')
+    print('##############################')
+    print(
+        f'Using {optimizer_id} with {criterion_id} on {model_id}'
+        f'with learning_rate: {learning_rate}.'
+    )
+    print('##############################')
     device = 'cuda:0'
 
     config = Config(
@@ -77,36 +72,29 @@ for model_id in models:
     )
 
     model = get_vit(config=config)
-    for criterion_id in ['l1']:
-        criterion = get_loss(criterion_id)
-        for optimizer_id in ['sgd']:
-            for learning_rate in learning_rates:
-                optimizer = get_optimizer(
-                    optimizer_id=optimizer_id,
-                    model=model,
-                    learning_rate=learning_rate
-                )
-                experiment_name = (
-                    f'{model_id}_lr-{learning_rate}_'
-                    f'loss-{criterion_id}_optim-{optimizer_id}')
-                print(f'Setting learning rate to: {learning_rate}')
-                print('##############################')
-                print(
-                    f'Using {optimizer_id} with {criterion_id} on {model_id}')
-                print('##############################')
-                trainer = Trainer(
-                    model_id=model_id,
-                    model=model,
-                    optimizer=optimizer,
-                    criterion=criterion,
-                    config=config,
-                    experiment_name=experiment_name,
-                    debug=debug,
-                    debug_step=debug_step,
-                    debug_image_count=debug_image_count
-                )
-                trainer.train(
-                    train_loader=train_loader,
-                    val_loader=val_loader,
-                    epochs=num_epochs
-                )
+    criterion = get_loss(criterion_id)
+    optimizer = get_optimizer(
+        optimizer_id=optimizer_id,
+        model=model,
+        learning_rate=learning_rate
+    )
+    experiment_name = (
+        f'{model_id}_lr-{learning_rate}_'
+        f'loss-{criterion_id}_optim-{optimizer_id}')
+
+    trainer = Trainer(
+        model_id=model_id,
+        model=model,
+        optimizer=optimizer,
+        criterion=criterion,
+        config=config,
+        experiment_name=experiment_name,
+        debug=debug,
+        debug_step=debug_step,
+        debug_image_count=debug_image_count
+    )
+    trainer.train(
+        train_loader=train_loader,
+        val_loader=val_loader,
+        epochs=num_epochs
+    )
